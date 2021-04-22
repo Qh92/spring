7bf01190e1db9218a05948518f7a103f0c83555c
Qh <771600967@qq.com>
2021-04-22 10:40:49 +0800

    feat:添加有参数构造器
    autowired注解作用在构造器上

--- spring5_annotation/src/main/java/com/qinh/dao/UserDaoImpl2.java
+++ spring5_annotation/src/main/java/com/qinh/dao/UserDaoImpl2.java
@@ -1,5 +1,6 @@
 package com.qinh.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,8 +10,18 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class UserDaoImpl2 implements UserDao{
+
+    private UserDao userDaoImpl;
+
+    @Autowired
+    public UserDaoImpl2(UserDao userDaoImpl){
+        this.userDaoImpl = userDaoImpl;
+    }
+
     @Override
     public void add() {
         System.out.println("UserDaoImpl2 add ....");
+        userDaoImpl.add();
     }
+
 }
